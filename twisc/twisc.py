10e23d2b967f9c19b2fac3182051ea7459f6cb47
pchchv <ipchchv@gmail.com>
2022-10-27 00:35:19 +0300

    Implement settings variable

--- twisc/twisc.py
+++ twisc/twisc.py
@@ -1,7 +1,11 @@
+import os
 import datetime
+from utils import init_driver, get_last_date_from_csv
 
 
-def scrape(since, until=None, interval=5):
+def scrape(since, until=None, init_date=None, max_date=None, words=None, to_account=None, from_account=None,
+           mention_account=None, interval=5, headless=True, resume=False, proxy=None, hashtag=None, show_images=False,
+           save_images=False, save_dir="outputs"):
     """
     Scrape data from twitter using requests.
     Starting from <since> until <until>.
@@ -28,3 +32,34 @@ def scrape(since, until=None, interval=5):
         until = datetime.date.today().strftime("%Y-%m-%d")
     # set refresh at 0. we refresh the page for each <interval> of time
     refresh = 0
+    # file path
+    if words:
+        if type(words) == str:
+            words = words.split("//")
+        path =\
+            save_dir + "/" + '_'.join(words) + '_' + str(since).split(' ')[0] + '_' + str(until).split(' ')[0] + '.csv'
+    elif from_account:
+        path = \
+            save_dir + "/" + from_account + '_' + str(since).split(' ')[0] + '_' + str(until).split(' ')[0] + '.csv'
+    elif to_account:
+        path = \
+            save_dir + "/" + to_account + '_' + str(since).split(' ')[0] + '_' + str(until).split(' ')[0] + '.csv'
+    elif mention_account:
+        path = \
+            save_dir + "/" + mention_account + '_' + str(init_date).split(' ')[0] + '_' + \
+            str(max_date).split(' ')[0] + '.csv'
+    elif hashtag:
+        path = \
+            save_dir + "/" + hashtag + '_' + str(since).split(' ')[0] + '_' + str(until).split(' ')[0] + '.csv'
+    # create the <save_dir>
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    # show images during scraping (for saving purpose)
+    if save_images:
+        show_images = True
+    # initiate the driver
+    driver = init_driver(headless, proxy, show_images)
+    # resume scraping from previous work
+    if resume:
+        since = str(get_last_date_from_csv(path))[:10]
+        write_mode = 'a'
