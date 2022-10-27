784347ab1acb2ff4796116de588e7afcd3b16921
pchchv <ipchchv@gmail.com>
2022-10-27 06:12:03 +0300

    Add __version__ file

---
+++ twisc/__version__.py
@@ -0,0 +1,7 @@
+__title__ = 'twisc'
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__author__ = 'pchchv'
+__contact__ = 'ipchchv@gmail.com'
+__url__ = 'https://github.com/pchchv/twisc'
+__description__ = 'Simple and unlimited Twitter scraper: tweets, likes, retweets, followers, user info, images, etc.'
